a666a789fa52006b04d90152512a504e4b73156e
JonatanFD <u20211b995@upc.edu.pe>
2025-04-24 00:41:43 -0500

    feat: add translations for login

---
+++ src/js/i18n/login/en.js
@@ -0,0 +1,15 @@
+const en = {
+    "login-title": "Login",
+    "back-button": "Go back to hub",
+    "user-icon": "User icon",
+    "tshirt-icon": "T-shirt icon",
+    "login-heading": "Login",
+    "email-label": "Username/Email:",
+    "email-placeholder": "Enter your username or email",
+    "password-label": "Password:",
+    "password-placeholder": "Enter your password",
+    "toggle-password-visibility": "Toggle password visibility",
+    "forgot-password-link": "Forgot password?",
+    "create-account-link": "Don’t have an account?",
+    "login-button": "LOGIN"
+};
---
+++ src/js/i18n/login/es.js
@@ -0,0 +1,15 @@
+const es = {
+    "login-title": "Iniciar sesión",
+    "back-button": "Volver al hub",
+    "user-icon": "Ícono de usuario",
+    "tshirt-icon": "Ícono de camiseta",
+    "login-heading": "Iniciar sesión",
+    "email-label": "Usuario/Correo electrónico:",
+    "email-placeholder": "Ingresa tu usuario o correo electrónico",
+    "password-label": "Contraseña:",
+    "password-placeholder": "Ingresa tu contraseña",
+    "toggle-password-visibility": "Alternar visibilidad de la contraseña",
+    "forgot-password-link": "¿Olvidaste tu contraseña?",
+    "create-account-link": "¿No tienes una cuenta?",
+    "login-button": "INICIAR SESIÓN"
+};
